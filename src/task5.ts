979b2227d365fa5b2283112b292532ae447fcad9
alman63 <mantulenko83@mail.ru>
2023-02-16 19:33:57 +0400

    Оправляю на проверку

--- src/task5.ts
+++ src/task5.ts
@@ -1,12 +1,17 @@
 // Есть функция которая достает из реакт компонента (любого, и Functional и Class) его defaultProps
 // Нужно заменить FIXME на правильный тип
-
+// import { React } from './react.js';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME = any;
+type FIXME<T> = T extends (...args: any[]) => infer R ? R : any;
 
 // Hint: infer
+namespace React {
+    export type ComponentType<T> = {
+        defaultProps;
+    };
+}
 export const getDefaultProps = <T>(
     component: React.ComponentType<T>
-): FIXME => {
+): FIXME<T> => {
     return component.defaultProps;
 };
--- src/task6.ts
+++ src/task6.ts
@@ -4,12 +4,21 @@
 // Из-за ограничений глубины вычислений поддержать все натуральные числа не получится, это нормально
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME = any;
 
 type Equals<A, B> = A extends B ? (B extends A ? 'success' : never) : never;
+type Increase<A, ACC extends Array<number> = []> = ACC['length'] extends A
+    ? [...ACC, 0]['length']
+    : Increase<A, [...ACC, 0]>;
+type Decrease<A, ACC extends Array<number> = []> = [
+    ...ACC,
+    0
+]['length'] extends A
+    ? ACC['length']
+    : Decrease<A, [...ACC, 0]>;
 
-type Add<A, B> = FIXME;
-type Subtract<A, B> = FIXME;
+type Add<A, B> = A extends 0 ? B : Add<Decrease<A>, Increase<B>>;
+type Subtract<A, B> = B extends 0 ? A : Subtract<Decrease<A>, Decrease<B>>;
 
 export type OnePlusOneTest = Equals<Add<1, 1>, 2>;
 export type TwoMinusOneTest = Equals<Subtract<2, 1>, 1>;
+export type TwoMinusTwoTest = Equals<Subtract<2, 2>, 0>;
