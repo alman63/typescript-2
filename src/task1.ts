89d0dbfefe24bda67909dd3fc076d5bf22dc41f9
alman63 <mantulenko83@mail.ru>
2023-03-05 16:47:36 +0400

    Update task1.ts

--- src/task1.ts
+++ src/task1.ts
@@ -4,23 +4,23 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 
 const orderStates = [
-    'initial',
-    'inWork',
-    'buyingSupplies',
-    'producing',
-    'fullfilled',
+  "initial",
+  "inWork",
+  "buyingSupplies",
+  "producing",
+  "fullfilled",
 ] as const;
 
-type FIXME = [Exclude<OrderState, 'buyingSupplies' | 'producing'>];
+type FIXME = [Exclude<OrderState, "buyingSupplies" | "producing">];
 // ['initial'|'inWork'|'fullfilled'];
 type OrderState = typeof orderStates[number];
 
 export const getUserOrderStates = (orderStates: OrderState[]): FIXME => {
-    const filteredStates = [] as FIXME;
-    orderStates.forEach((element) => {
-        if (element !== 'buyingSupplies' && element !== 'producing') {
-            filteredStates.push(element);
-        }
-    });
-    return filteredStates;
+  const filteredStates = [] as FIXME;
+  orderStates.forEach((element) => {
+    if (element !== "buyingSupplies" && element !== "producing") {
+      filteredStates.push(element);
+    }
+  });
+  return filteredStates;
 };
