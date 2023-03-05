f619953c38c787d243f92bb692fa38547d1ee50e
alman63 <mantulenko83@mail.ru>
2023-03-05 16:45:39 +0400

    Update task1.ts

--- src/task1.ts
+++ src/task1.ts
@@ -2,24 +2,25 @@
 // Нужно заменить FIXME на тип который вычисляется на освове OrderState
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME = [Exclude<OrderState, "buyingSupplies" | "producing">];
-// ['initial'|'inWork'|'fullfilled'];
+
 const orderStates = [
-  "initial",
-  "inWork",
-  "buyingSupplies",
-  "producing",
-  "fullfilled",
+    'initial',
+    'inWork',
+    'buyingSupplies',
+    'producing',
+    'fullfilled',
 ] as const;
 
+type FIXME = [Exclude<OrderState, 'buyingSupplies' | 'producing'>];
+// ['initial'|'inWork'|'fullfilled'];
 type OrderState = typeof orderStates[number];
 
 export const getUserOrderStates = (orderStates: OrderState[]): FIXME => {
-  const filteredStates = [] as FIXME;
-  orderStates.forEach((element) => {
-    if (element !== "buyingSupplies" && element !== "producing") {
-      filteredStates.push(element);
-    }
-  });
-  return filteredStates;
+    const filteredStates = [] as FIXME;
+    orderStates.forEach((element) => {
+        if (element !== 'buyingSupplies' && element !== 'producing') {
+            filteredStates.push(element);
+        }
+    });
+    return filteredStates;
 };
