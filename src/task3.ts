c9e4040ff2689661904baae18729c14234689ec0
alman63 <mantulenko83@mail.ru>
2023-03-06 14:14:42 +0400

    исправил ошибки после созвона

--- src/task3.ts
+++ src/task3.ts
@@ -2,15 +2,13 @@
 // Нужно заменить FIXME на соответствующий тип
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME<T> = {
-  [x: string]: T;
-};
+type FIXME<T> = Record<any, T>;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const omit = <T extends Record<any, any>, K extends keyof T>(
   obj: T,
   keyToOmit: K
-): FIXME<Record<any, any>> => {
+): FIXME<T> => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { [keyToOmit]: _, ...withoutKey } = obj;
   return withoutKey;
--- src/task5.ts
+++ src/task5.ts
@@ -1,15 +1,16 @@
 // Есть функция которая достает из реакт компонента (любого, и Functional и Class) его defaultProps
 // Нужно заменить FIXME на правильный тип
 // import { React } from './react.js';
+import React from "react";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME<T> = T extends (...args: any[]) => infer R ? R : any;
+type FIXME<T> = Partial<React.ComponentType<T>> | undefined;
+// Omit<
+//     React.Component<T>,
+//     Exclude<React.Component<T>, 'defaultProps'>
+// >;
 
 // Hint: infer
-declare namespace React {
-  export type ComponentType<T> = {
-    defaultProps: T extends (...args: any[]) => infer R ? R : any;
-  };
-}
+
 export const getDefaultProps = <T>(
   component: React.ComponentType<T>
 ): FIXME<T> => {
